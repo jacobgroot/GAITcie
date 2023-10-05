17c3af309b4d84f4eaef9a5d5b0391bf481e259f
Jacob Groot <jacobgroot79@gmail.com>
2023-10-05 15:12:37 +0200

    Structuur en eerste poging gebruik maken van API

---
+++ filter.py
@@ -0,0 +1,1 @@
+'''This file filters all non person pictures and all videos out of dataset'''
---
+++ main.py
@@ -0,0 +1,15 @@
+'''Imports filtered images and calls facial recognition, than saves the files to local system.  '''
+
+# API key: vgDq8WpPDCKdWTM8F9bX5d6HdT8mRRvD
+# API Secret: XVXkQGKsFpJk9tHbMJHZdZgTBNbLrgCjW3f3XL4hQLFLbjHJq6QvLc64h82Zzp2d
+
+class Load(): 
+    def __init__ (API_key, API_secret):
+        pass
+
+class Filter(): ...
+
+class Proces(): ...
+"AI dingen"
+
+class Upload(): ... 
