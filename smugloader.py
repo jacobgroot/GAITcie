b3cf2a9acde32580ff9073dc1ed1c96eea2a67e3
Jacob Groot <jacobgroot79@gmail.com>
2023-10-06 18:21:06 +0200

    finished downloader

--- smugloader.py
+++ smugloader.py
@@ -1,4 +1,5 @@
 import requests
+import os
 from requests_oauthlib import OAuth1Session, OAuth1
 
 class Connection():
@@ -19,48 +20,91 @@ class Connection():
         ACCESS_TOKEN_URL = 'https://api.smugmug.com/services/oauth/1.0a/getAccessToken'
         OAUTH_CALLBACK = 'oob'  # non-web application
 
-        # Create an OAuth1Session with your API credentials and the callback parameter
+        # use OAuth to create session
         oauth = OAuth1Session(self.API_KEY, client_secret=self.API_SECRET, callback_uri=OAUTH_CALLBACK)
 
-        # Get a request token and token secret
+        # Get request token and token secret
         fetch_response = oauth.fetch_request_token(REQUEST_TOKEN_URL)
 
-        # Extract the request token and secret
+        # Extract request token and secret
         request_token = fetch_response.get('oauth_token')
         request_token_secret = fetch_response.get('oauth_token_secret')
 
-        # Redirect the user to the authorization URL
+        # Redirect user to the authorization URL
         authorization_url = oauth.authorization_url(AUTHORIZE_URL)
-        print("Please go to the following URL and authorize the application:", authorization_url)
-        verifier = input("Enter the verifier: ")
+        print("Open URL and authorize application:", authorization_url)
+        verifier = input("Enter the SmugMug number: ")
 
-        #Get the access token and access token secret
+        # Get access token secret
         oauth = OAuth1Session(self.API_KEY, client_secret=self.API_SECRET, resource_owner_key=request_token, resource_owner_secret=request_token_secret, verifier=verifier)
         access_token_response = oauth.fetch_access_token(ACCESS_TOKEN_URL)
 
-        # Extract the access token and secret
+        # Extract access token and secret
         self.ACCES_TOKEN = access_token_response.get('oauth_token')
         self.ACCES_TOKEN_SECRET = access_token_response.get('oauth_token_secret')
-        print(self.ACCES_TOKEN)
-
 
 class Loader(Connection):
-    '''Loads pictures from smugmug, uses access token that Connection can fetch'''
+    ''' Loads pictures from smugmug, uses access token that Connection can fetch
+        Does not download videos. If you also want to download videos, turn self.__download_vid True
+        If you only want to download videos, turn self.__download_img off'''
+    
     def __init__(self, API_KEY, API_SECRET, ACCES_TOKEN, ACCES_TOKEN_SECRET):
         super().__init__(API_KEY, API_SECRET, ACCES_TOKEN, ACCES_TOKEN_SECRET)
-        self.url = 'https://api.smugmug.com/api/v2/album'
+        self.__download_img = True
+        self.__download_video = False
+        self.alb_url = 'https://api.smugmug.com/api/v2/folder/user/rubenlugters!albumlist'
+        self.img_base_url = 'https://api.smugmug.com/api/v2/album/' 
+        self.download_directory = os.path.join(os.getcwd(), 'media')
+
+        # i have the media path, but when Gerstenat wants to use it, they might not have it
+        if not os.path.exists(self.download_directory):
+            os.makedirs(self.download_directory)
+
+        self.images = []
 
     def download(self):
+        '''Performs downloading operation. Uses self.__download params to specify downloading type'''
         auth = OAuth1(
             self.API_KEY,
             client_secret=self.API_SECRET,
             resource_owner_key=self.ACCES_TOKEN,
             resource_owner_secret=self.ACCES_TOKEN_SECRET
         )
+        headers = {'Content-type': 'application/json', 'Accept': 'application/json', 'Methods': "GET"}
+        response_raw = requests.get(self.alb_url, auth=auth, headers=headers)
 
-        response = requests.get(self.url, auth=auth)
-        if response.status_code != "200":
+        if response_raw.status_code != "200":
             self._get_acces
-        print(response.status_code)
+
+        response = response_raw.json()
+
+        for album in response["Response"]["AlbumList"]:
+            images_url = 'https://api.smugmug.com' + album["Uri"] + "!images"
+            imgs = requests.get(images_url, auth=auth, headers=headers).json()
+            for pic in imgs["Response"]["AlbumImage"]:
+                self.__download_media(pic["ArchivedUri"])
+            raise
+
+    def __download_media(self, image_url):
+        """ Downloads images and videos, on conditions of params __download
+            Writes all media to download_directory"""
+
+        image_name = image_url.split("/")[-1]  # Extract the image name from the URL
+        image_path = os.path.join(self.download_directory, image_name)
+
+        # perform download type check
+        if not self.__download_video and image_name[:3] != "IMG":
+            return
+        if not self.__download_img and image_name[:3] == "IMG":
+            return 
+        
+        response = requests.get(image_url)
+        if response.status_code == 200:
+            with open(image_path, 'wb') as f:
+                f.write(response.content)
+            print(f"Downloaded: {image_name}")
+        else:
+            print(f"Failed to download: {image_name}")
+
 
 
