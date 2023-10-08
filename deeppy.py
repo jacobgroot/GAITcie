960287ae014d56aa85c56e44131e9938e2456ea0
Jacob Groot <jacobgroot79@gmail.com>
2023-10-08 11:08:01 +0200

    comments on deeppy

--- deeppy.py
+++ deeppy.py
@@ -1,10 +1,10 @@
 """
-File calls deepface on all images in folder media/.
-!Important! media/ should always be the folder containing images/videos to be analyzed.
-The file will create a temporary file with all embeddings (overwriting the previous embedding file).
+File contains data architecture and analysis class to create embeddings for images containing persons
+those embeddings will be safed to a file (name specified when called and included in .gitignore) and can be used to sort images per person
 """
 
 import os
+import multiprocessing
 import pickle
 from tqdm import tqdm
 from deepface import DeepFace
@@ -17,26 +17,37 @@ class Faces:
     image_path: str
     labels: list = field(default_factory=list)
 
-# store all Faces instances
-faces = []
-local_faces = []
-json_filename = "deepface_data.json"
+class Analyse():
+    """ 
+    Class analyses every image in the media/ folder with the DeepFace import.
+    Creates a pickle file with all the embeddings and the corresponding image
+    """
 
-# Remove the previous JSON file if it exists
-if os.path.exists(json_filename):
-    os.remove(json_filename)
+    # CHANGE THIS WHEN YOU RUN ON DIFFERENT PC TO NOT BLOW IT UP
+    NUMBER_OF_CORES = 2
 
-# call DeepFace on each image
-media_path = os.path.join(os.getcwd(), 'media')
-for filename in tqdm(os.listdir(media_path)):
-    img_path = os.path.join(media_path, filename)
-    embeddings = [x["embedding"] for x in DeepFace.represent(img_path, enforce_detection=False)]
-    faces.append(Faces(embeddings, img_path))
-    local_faces.append({
-        "embeddings": embeddings,
-        "img_path": img_path
-    })
+    def __init__(self, pickle_file):
+        self.faces = []
+        self.pickle_file = pickle_file
+        self.media_path = os.path.join(os.getcwd(), 'media')
 
-# Save the data to a pickle file
-with open(json_filename, 'wb') as pickle_file:
-    pickle.dump(faces, pickle_file)
+        # Remove the previous pickle file
+        if os.path.exists(pickle_file):
+            os.remove(pickle_file)
+
+    def process_image(img_path):
+        " Returns Faces dataclass with all embeddings per image (n=number of persons in image)"
+
+        embeddings = [x["embedding"] for x in DeepFace.represent(img_path, enforce_detection=False)]
+        return Faces(embeddings, img_path)
+
+    def analyse(self):
+        " Calls process_image on all images in media folder and stores it in pickle file"
+        image_files = [os.path.join(self.media_path, filename) for filename in os.listdir(self.media_path)]
+
+        for img in tqdm(image_files):
+            self.faces.append(self.process_image(img))
+
+        # Save the results to a pickle file
+        with open(self.pickle_file, "wb") as file:
+            pickle.dump(self.faces, file)
--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 '''Imports filtered images and calls facial recognition, than saves the files to local system.  '''
 from smugloader import Loader
+from 
 from api import API_KEY, API_SECRET, ACCES_TOKEN, ACCES_TOKEN_SECRET
 
 DOWNLOAD = False
-
+DEEPFACE = False
 
 if DOWNLOAD:
     loaded_content = Loader(API_KEY, API_SECRET, ACCES_TOKEN, ACCES_TOKEN_SECRET)
     loaded_content.download()
+
+if DEEPFACE:
 
 class Proces(): ...
 "AI dingen"
