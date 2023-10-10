a3ed21d7daf825232fc35dca6336e2131e47026d
Jacob Groot <jacobgroot79@gmail.com>
2023-10-10 10:06:04 +0200

    normal version

--- kmeans.py
+++ kmeans.py
@@ -35,28 +35,6 @@ class Kmeans_cluster():
         # generate new embeddings
         umap_embedding = umap_model.fit_transform(embedding_list)
 
-        x_coords, y_coords, z_coords = zip(*umap_embedding)
-
-        # Create a 3D scatter plot
-        fig = plt.figure(figsize=(8, 8))
-        ax = fig.add_subplot(111, projection='3d')
-        ax.scatter(x_coords, y_coords, z_coords, marker='o', s=10)
-
-        # Optionally, you can label the points with face IDs or other attributes
-        for i, (x, y, z) in enumerate(umap_embedding):
-            ax.text(x, y, z, f"Face {i}", fontsize=8)
-
-        ax.set_title("3D UMAP Embeddings")
-        ax.set_xlabel("UMAP Dimension 1")
-        ax.set_ylabel("UMAP Dimension 2")
-        ax.set_zlabel("UMAP Dimension 3")
-        ax.grid(True)
-
-        mng = plt.get_current_fig_manager()
-        mng.window.showMaximized()
-
-        plt.savefig("3d_plot_umap")
-        plt.show()
         # store embeddings
         for i, face in enumerate(self.faces):
             face.umap_embedding = umap_embedding[i]
@@ -78,16 +56,15 @@ class Kmeans_cluster():
             self.faces = pickle.load(file)
         umap_embeddings = [face.umap_embedding for face in self.faces]
 
-        start, stop = self.range
-        x_values = range(start, stop)
-        imgs = []
-        clusters = DBSCAN(eps=0.0005, min_samples=1, metric='cosine').fit(umap_embeddings)
+        clusters = DBSCAN(eps=0.00005, min_samples=1, metric='cosine').fit(umap_embeddings)
         print(clusters.labels_)
-        output_folder = os.path.join(os.getcwd(), "labeled_pictures")
-        for i, face in enumerate(self.faces):
-            face.label = clusters.labels_[i]
-            if i == 16 or i == 14 or i == 92 or i == 18 or i == 94 or i == 6 or i == 84: 
-                shutil.copy(face.image_path, output_folder)
+
+        # # safe in specific cases for testing
+        # output_folder = os.path.join(os.getcwd(), "labeled_pictures")
+        # for i, face in enumerate(self.faces):
+        #     face.label = clusters.labels_[i]
+        #     if i == 16 or i == 14 or i == 92 or i == 18 or i == 94 or i == 6 or i == 84: 
+        #         shutil.copy(face.image_path, output_folder)
 
     def plot_elbow(self, inertia):
         start, stop = self.range
